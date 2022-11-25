836d9441d0d9d0cb90aff8ddd37112ce2d1388ef
13232160100 <ypc0100@qq.com>
2022-11-25 23:19:19 +0800

    233

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,21 +2,7 @@
   <div class="login_container">
     <div class="login_box">
       <div class="login_form">
-        <div style="margin: 20px 0; text-align: center; font-size: 34px; color:white;"><b>工具集</b></div>
-        <el-form :model="user" :rules="rules" ref="userForm">
-          <el-form-item prop="username">
-            <el-input size="large" style="margin: 10px 0; width: 400px" prefix-icon="el-icon-user"
-              v-model="user.username"></el-input>
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input size="large" style="margin: 10px 0; width: 400px" prefix-icon="el-icon-lock" show-password
-              v-model="user.password"></el-input>
-          </el-form-item>
-          <el-form-item style="margin: 10px 0; text-align: center">
-            <!--          <el-button type="warning" size="small"  autocomplete="off" @click="$router.push('/register')">注册</el-button>-->
-            <el-button type="primary" size="medium" autocomplete="off" @click="login" style="width:100px">登录</el-button>
-          </el-form-item>
-        </el-form>
+
       </div>
     </div>
     <vue-particles color="#409EFF" :particleOpacity="0.7" :particlesNumber="80" shapeType="circle" :particleSize="4"
@@ -57,18 +43,5 @@ methods: {
       top: 50%;
       transform: translate(-50%, -50%);
   }
-  .login_form{
-      background-color: #fff;
-      width: 600px;
-      height: 320px;
-      padding: 20px;
-      border-radius: 10px;
-      position: absolute;
-      background-color: rgba(0, 0, 0, 0.3);
-      left: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 2;
-      box-shadow: 10px 10px 20px 0px rgba(0, 0, 0, 0.7);
-      text-align: center;
-  }
+
 </style>
