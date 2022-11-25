d41a5983b35aeab58b69a2422c25dc3040429707
13232160100 <ypc0100@qq.com>
2022-11-25 23:10:02 +0800

    2233

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,7 +3,7 @@
     <div class="login_box">
       <div class="login_form">
         <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-          <h3 class="title">工具集</h3>
+          <!-- <h3 class="title">工具集</h3> -->
           <el-form-item prop="username">
             <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
               <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
@@ -26,8 +26,8 @@
           </el-form-item>
           <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
           <el-form-item style="width:100%;">
-            <el-button :loading="loading" size="medium" type="primary" style="width:100%;"
-              @click.native.prevent="handleLogin">
+            <el-button :loading="loading" size="medium" type="primary" style="width:100%;background-color: rgba(0, 0, 0, 0.3);"
+              @click.native.prevent="handleLogin" plain>
               <span v-if="!loading">登 录</span>
               <span v-else>登 录 中...</span>
             </el-button>
