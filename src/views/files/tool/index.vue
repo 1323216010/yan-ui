0fd9bf9e8424a59c3bca3a33ef4e65e4b7c88215
13232160100 <ypc0100@qq.com>
2022-12-01 00:02:38 +0800

    翻译界面

---
+++ src/views/files/tool/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <el-input style="max-width: 450px;position: absolute;left:20%;top:20%;transform: translate(-50%, -50%);" :autosize="{ minRows: 8, maxRows: 4}" type="textarea" placeholder="请输入翻译内容" v-model="textarea" clearable>
+        </el-input>
+        <el-input style="max-width: 450px;position: absolute;right:20%;top:20%;transform: translate(50%, -50%);" :autosize="{ minRows: 8, maxRows: 4}" type="textarea" placeholder="请输入翻译内容" v-model="result" clearable>
+        </el-input>
+    </div>
+</template>
+  
+<script>
+  
+  export default {
+  name: "tool",
+  data() {
+    return {
+      user: {},
+      textarea: '',
+      result: ''
+    }
+  },
+  methods: {
+  
+  }
+  }
+  </script>
+  
+<style scoped>
+
+</style>
