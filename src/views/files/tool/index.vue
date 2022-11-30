5de0c7b01139fd2ee217706fa0edf155b171050f
13232160100 <ypc0100@qq.com>
2022-12-01 00:08:32 +0800

    调整页面

--- src/views/files/tool/index.vue
+++ src/views/files/tool/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <el-input style="max-width: 450px;position: absolute;left:20%;top:20%;transform: translate(-50%, -50%);" :autosize="{ minRows: 8, maxRows: 4}" type="textarea" placeholder="请输入翻译内容" v-model="textarea" clearable>
+        <el-input style="max-width: 450px;position: absolute;left:25%;top:35%;transform: translate(-50%, -50%);" :autosize="{ minRows: 8, maxRows: 4}" type="textarea" placeholder="请输入翻译内容" v-model="textarea" clearable>
         </el-input>
-        <el-input style="max-width: 450px;position: absolute;right:20%;top:20%;transform: translate(50%, -50%);" :autosize="{ minRows: 8, maxRows: 4}" type="textarea" placeholder="请输入翻译内容" v-model="result" clearable>
+        <el-input style="max-width: 450px;position: absolute;right:25%;top:35%;transform: translate(50%, -50%);" :autosize="{ minRows: 8, maxRows: 4}" type="textarea" placeholder="请输入翻译内容" v-model="result" clearable>
         </el-input>
     </div>
 </template>
