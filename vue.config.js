108aaeadb88c63face424efd76cb3bd2983fb308
13232160100 <101637168+1323216010@users.noreply.github.com>
2022-12-05 17:03:25 +0800

    更新地址

--- vue.config.js
+++ vue.config.js
@@ -33,14 +33,13 @@ module.exports = {
     port: port,
     open: true,
     proxy: {
-      // detail: https://cli.vuejs.org/config/#devserver-proxy
-      [process.env.VUE_APP_BASE_API]: {
-        target: `http://127.0.0.1:8080`,
+      '^/api': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
         pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
-        }
-      }
+          '^/api': '/',
+        },
+      },
     },
     disableHostCheck: true
   },
